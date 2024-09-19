c13f8720665b1baa9888ec8f41acc3589d9e21ef
Evaline <evaline@relugames.com>
2024-09-19 15:35:04 +0900

    최종 점수 반영

--- Assets/Scripts/STTScripts/DecisionSystem.cs
+++ Assets/Scripts/STTScripts/DecisionSystem.cs
@@ -1,20 +1,78 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DecisionSystem : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI questionText;
+    [SerializeField] private UnityEngine.UI.Button newQuestionButton;
+    [SerializeField] private UnityEngine.UI.Button recordButton;
+    [SerializeField] private UnityEngine.UI.Image recordImage;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private UnityEngine.UI.Slider scoreSlider;
+
+    [SerializeField] private Sprite recordingSprite;
+    [SerializeField] private Sprite notRecordingSprite;
+    [FormerlySerializedAs("perfectScore")] [SerializeField] private int perfectOneRoundScore = 100;
+    [SerializeField] private float finalScore = 1000;
+    
     [SerializeField] private MicrophoneRecorder microphoneRecorder;
     [SerializeField] private List<string> answerBook = new List<string>();
+
+    private bool isRecording = false;
+    private string answerString = string.Empty;
+    private float totalScore = 0;
     private void Start()
     {
+        answerString = questionText.text;
+        
+        newQuestionButton.onClick.AddListener(SetNewQuestion);
+        
+        recordButton.onClick.AddListener(() =>
+        {
+            if (!isRecording)
+            {
+                StartRecording();
+            }
+            else
+            {
+                StopRecording();
+            }
+        });
+        
         microphoneRecorder.transcriptionCompleteCallback += OnTranscriptionComplete;
+        recordImage.sprite = notRecordingSprite;
+        scoreSlider.value = 0;
+    }
+
+    private void SetNewQuestion()
+    {
+        answerString = answerBook[UnityEngine.Random.Range(0, answerBook.Count)];
+        questionText.text = answerString;
+    }
+
+    private void StartRecording()
+    {
+        isRecording = true;
+        recordImage.sprite = recordingSprite;
+        microphoneRecorder.StartRecording();
+    }
+
+    private void StopRecording()
+    {
+        isRecording = false;
+        recordImage.sprite = notRecordingSprite;
+        microphoneRecorder.StopRecording();
     }
 
     private void OnTranscriptionComplete(string inTranscribedString)
     {
+        int score = Mathf.Max(0, perfectOneRoundScore - SimpleCharacterDifference(inTranscribedString, answerString));
+        scoreText.text = score.ToString();
+        totalScore += score;
         
+        scoreSlider.value = totalScore / finalScore;
     }
 
     private int SimpleCharacterDifference(string a, string b)
