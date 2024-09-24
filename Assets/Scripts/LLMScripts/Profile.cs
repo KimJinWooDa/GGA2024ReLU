fa098f5dc8d3abf004cf948a0c014bf6b9cab537
Evaline <evaline@relugames.com>
2024-09-24 14:12:10 +0900

    UI 수정

--- Assets/Scripts/LLMScripts/Profile.cs
+++ Assets/Scripts/LLMScripts/Profile.cs
@@ -1,13 +1,28 @@
+using System;
 using UnityEngine;
 
 public class Profile : MonoBehaviour
 {
-    [SerializeField] private GameObject selectedIndicator;
-    [SerializeField] private UnityEngine.UI.Image profileImage;
-    [SerializeField] private string information;
-
+    public Action<Profile> OnSelected;
     public bool IsSelected = false;
     
+    [TextArea]
+    public string information;
+    public Sprite profileSprite;
+    
+    [SerializeField] private GameObject selectedIndicator;
+    [SerializeField] private UnityEngine.UI.Image profileImage;
+    [SerializeField] private UnityEngine.UI.Button profileButton;
+    
+    private void Start()
+    {
+        profileImage.sprite = profileSprite;
+        profileButton.onClick.AddListener(() =>
+        {
+            OnSelected?.Invoke(this);
+        });
+    }
+
     public void SetSelected(bool selected)
     {
         IsSelected = selected;
